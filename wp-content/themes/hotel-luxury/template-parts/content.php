8106ca3ab2f6b0d597f8f68dee5beb2ef6f661d7
coolJenny <coolJenny009g@gmail.com>
2018-03-22 22:25:18 -0400

    ..

--- wp-content/themes/hotel-luxury/template-parts/content.php
+++ wp-content/themes/hotel-luxury/template-parts/content.php
@@ -8,24 +8,25 @@
  */
 
 ?>
-
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <div class="post-heading">
-        <div class="blog-meta">
-			<?php hotel_luxury_posted_on(); ?>
+<div class="col-md-5">
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <div class="post-heading">
+            <div class="blog-meta">
+                <?php hotel_luxury_posted_on(); ?>
+            </div>
         </div>
-    </div>
 
-	<?php if ( has_post_thumbnail() ) { ?>
+        <?php if ( has_post_thumbnail() ) { ?>
         <div class="blog-thumb-wrapper">
             <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-				<?php the_post_thumbnail( 'fashify-thumb-default' ); ?>
+                <?php the_post_thumbnail( 'fashify-thumb-default' ); ?>
             </a>
         </div>
-	<?php } ?>
+        <?php } ?>
 
-    <div class="text-content">
-        <?php the_content() ; ?>
-    </div>
+        <div class="text-content">
+            <?php the_content() ; ?>
+        </div>
 
-</article><!-- #post-<?php the_ID(); ?> -->
+    </article><!-- #post-<?php the_ID(); ?> -->
+</div>
