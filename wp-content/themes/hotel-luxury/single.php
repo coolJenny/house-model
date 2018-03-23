ac987edf8261f493b2b296479fdbef04b6f75c73
coolJenny <coolJenny009g@gmail.com>
2018-03-22 22:35:19 -0400

    ..

--- wp-content/themes/hotel-luxury/single.php
+++ wp-content/themes/hotel-luxury/single.php
@@ -14,22 +14,35 @@ get_header(); ?>
 
     <div id="primary" class="content-area row">
         <div class="content-wrapper col-md-12">
-            <main id="main" class="site-main">
+            <main id="main" class="site-main col-md-12">
+                <div class="row">
+                    <?php
+                    while ( have_posts() ) : the_post();
 
-            <?php
-            while ( have_posts() ) : the_post();
+                        get_template_part( 'template-parts/content', get_post_type() );
+                        ?>
 
-                get_template_part( 'template-parts/content', get_post_type() );
+                        <div class="col-md-6">
 
-                the_post_navigation();
+                            <?php
+                            the_post_navigation();
 
-                // If comments are open or we have at least one comment, load up the comment template.
-                if ( comments_open() || get_comments_number() ) :
-                    comments_template();
-                endif;
+                            // If comments are open or we have at least one comment, load up the comment template.
+                            if ( comments_open() || get_comments_number() ) :
+                                comments_template();
+                            endif;
+                            ?>
+                            <div class="post-heading">
+                                <div class="blog-meta">
+                                    <?php hotel_luxury_posted_on(); ?>
+                                </div>
+                            </div>
+                            <?php
 
-            endwhile; // End of the loop.
-            ?>
+                    endwhile; // End of the loop.
+                    ?>
+                        </div>
+                </div>                
             </main><!-- #main -->
         </div>
 
--- wp-content/themes/hotel-luxury/template-parts/content.php
+++ wp-content/themes/hotel-luxury/template-parts/content.php
@@ -8,13 +8,8 @@
  */
 
 ?>
-<div class="col-md-5">
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <div class="post-heading">
-            <div class="blog-meta">
-                <?php hotel_luxury_posted_on(); ?>
-            </div>
-        </div>
+<div class="col-md-6">
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>        
 
         <?php if ( has_post_thumbnail() ) { ?>
         <div class="blog-thumb-wrapper">
