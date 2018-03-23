cbe97753fcd23b05abbcfdb83a5e21f8b9a60e73
coolJenny <coolJenny009g@gmail.com>
2018-03-22 22:36:10 -0400

    ..

--- wp-content/themes/hotel-luxury/single.php
+++ wp-content/themes/hotel-luxury/single.php
@@ -26,11 +26,6 @@ get_header(); ?>
 
                             <?php
                             the_post_navigation();
-
-                            // If comments are open or we have at least one comment, load up the comment template.
-                            if ( comments_open() || get_comments_number() ) :
-                                comments_template();
-                            endif;
                             ?>
                             <div class="post-heading">
                                 <div class="blog-meta">
@@ -38,6 +33,11 @@ get_header(); ?>
                                 </div>
                             </div>
                             <?php
+
+                            // If comments are open or we have at least one comment, load up the comment template.
+                            if ( comments_open() || get_comments_number() ) :
+                                comments_template();
+                            endif;                            
 
                     endwhile; // End of the loop.
                     ?>
